616eed2dece6ac64b849178d1b69c12cee51feee
Shatakshi <shatakshiixb@gmail.com>
2026-09-16 09:51:54 +0530

    Time: 30 ms (51.28%) | Memory: 44.4 MB (18.62%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
 
-    int helper(string &s, string &t,vector<vector<int>>& dp, int i , int j){
-        if(j == 0) return 1;
-        if(i == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i-1] == t[j-1]){
-            return dp[i][j] = helper(s,t,dp,i-1,j-1) + helper(s,t,dp,i-1,j);
-        }
-        return helper(s,t,dp,i-1,j);
-    }
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return helper(s,t,dp,m,n);
+        vector<vector<unsigned long long>>dp(m+1,vector<unsigned long long>(n+1,0));
+        for(int i = 0 ; i <= m ; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1 ; i <= m ; i++){
+            for(int j = 1; j <= n ; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+        // dp state = no. of distinct subsequences generated from first j characters of t while considering first i chaters of s.
     }
 };
